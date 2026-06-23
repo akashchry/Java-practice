8e45f1cf5646b19b1c6a9a8fc92237ca89ab0c8e
akashchry <akashchoudhary1218@gmail.com>
2026-06-23 14:12:59 +0530

    Java Practice

--- src/Basics/VariablesDemo.java
+++ src/Basics/VariablesDemo.java
@@ -1,4 +1,19 @@
 package Basics;
 
 public class VariablesDemo {
+    public static void main(String[] args) {
+
+    int age = 22;
+    double salary = 10000.5;
+    char grade = 'A';
+    boolean status = true;
+    String name = "Akash";
+
+
+        System.out.println("Age = " + age);
+        System.out.println("Salary = " + salary);
+        System.out.println("Grade = " + grade);
+        System.out.println("Status = " + status);
+        System.out.println("Name = " + name);
+    }
 }
