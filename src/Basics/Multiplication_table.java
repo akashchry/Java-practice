f47c4c3d741d6f44c458f25479835e42b0040f44
akashchry <akashchoudhary1218@gmail.com>
2026-06-25 15:29:42 +0530

    Java Practice

---
+++ src/Basics/Multiplication_table.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Multiplication_table {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 1; i<=10; i++){
+            System.out.println(n + "*" + i + "=" +(n*i) );
+        }
+    }
+}
---
+++ src/Basics/Sum_of_NUmber.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Sum_of_NUmber {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i =1;i<=5;i++){
+            sum+=i;
+//            System.out.println(sum);
+      }
+        System.out.println(sum);
+    }
+}
